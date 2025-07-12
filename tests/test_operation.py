7ad82584b85353330123a7a913de15b28ca32b28
Matthaios Dimitrios Tzimas <mathewtzimas@gmail.com>
2025-07-12 17:42:17 +0300

    add minus test cases

---
+++ tests/test_operation.py
@@ -0,0 +1,13 @@
+from src.math_operation import * 
+
+def test_add():
+    assert add(5,4)==9
+    assert add(5,6)==10
+    assert add(4,4)==8
+    
+def test_minus():
+    assert minus(3,2)==1
+    assert minus(5,2)==3
+    
+    
+    
